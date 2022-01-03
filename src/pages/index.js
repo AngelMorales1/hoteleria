6b21135b9ad4babf6633d1399f465c5108c615b8
Angel Morales <angelmoraleszero@gmail.com>
2022-01-03 19:34:15 -0300

    tailwind con yarn

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-        <h1 className='font-sans  hover:font-serif'>sadsad</h1>
+        <h1 className='font-sans  hover:font-serif'>sadsadasdad</h1>
         <h2>ghgfgfgfg</h2>
     </div>
   )
